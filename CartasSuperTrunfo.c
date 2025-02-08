8ff84facb8d08782a332171e7a429d1ec123c961
joaovitorl10 <jvitorcardoso056@gmail.com>
2025-02-08 19:13:38 +0000

    terminei o aventureiro e mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,63 +1,329 @@
 #include <stdio.h>
 
-int main() { 
-char nome[50];   Barueri  
- char codigo[4];   A01
- int populacao;    316.473
-float area km²;    64,17 
-float pib;         4500000000
-int pontosTuristicos; 12
+// Definição da estrutura para armazenar informações da cidade
+struct Cidade {
+    char estado[50]; Acre
+    char nome[50]; Rio Branco
+    char codigo[4];A01 // Código da cidade (ex: A01)
+    int populacao; 364.756 
+    float area; 9.233 km² 
+    float pib; 10.955675 
+    int pontos_turisticos;64
 
-char nome[50];  São Paulo
-char codigo[4]; A02
-int populacao; 11450000
-float area km²; 1.523 
-float pib;     699280000000
-int pontosTuristicos; 250
+    char estado[50]; Acre
+    char nome[50]; Cruzeiro do Sul
+    char codigo[4];A02
+    int populacao; 12 457
+    float area;  7 924,943 km²
+    float pib; 1,425,684,780 
+    int pontos_turisticos; 10
 
-char nome[50]; Avanhandava
-char codigo[4]; A03
-int populacao; 14063
-float area km²; 340,338 
-float pib;  199400000
-int pontosTuristicos; 10
+    char estado[50]; Acre
+    char nome[50]; Mancio Lima
+    char codigo[4];A03
+    int populacao; 17.910 
+    float area;  4 672,321 km²
+    float pib;  88 211,169 
+    int pontos_turisticos; 7
 
-char nome[50]; União Paulista
-char codigo[4]; A04
-int populacao; 1 603
-float area km²; 79,1 
-float pib; 35 925 
-int pontosTuristicos; 10
+   char estado[50]; Acre
+    char nome[50]; Plácido de Castro
+    char codigo[4];A04
+    int populacao;  19 565
+    float area;  2.047 km²
+    float pib;   601.471
+    int pontos_turisticos; 5
 
- printf("Insira os dados para a cidade %s (%s):\n", cidade,nome, cidade,codigo);
 
- printf("População: ");
-    scanf("%d", &cidade,populacao);
+
+   char estado[50]; Bahia
+    char nome[50]; Salvador
+    char codigo[4]; B01
+    int populacao;  2.910705
+    float area;  693,442 km²
+    float pib; 114.400000000
+    int pontos_turisticos; 15
+
+   char estado[50]; Bahia
+    char nome[50]; Feira de Santana
+    char codigo[4]; B02
+    int populacao;  616.272 
+    float area;  1.304 km²
+    float pib; 27.691,08
+    int pontos_turisticos; 12
+
+    char estado[50]; Bahia
+   char nome[50];  Ilheus
+    char codigo[4]; B03
+    int populacao;  178.649
+    float area; 1.456 km²
+    float pib; 32.756
+    int pontos_turisticos; 15
+    
+    char estado[50]; Bahia
+    char nome[50];  Porto Seguro
+    char codigo[4]; B04
+    int populacao; 168.326
+    float area;2 285,734 km²
+    float pib;  24.702,76.
+    int pontos_turisticos; 14
+ 
+
+
+
+    char estado[50]; Ceará
+    char nome[50]; Fortaleza
+    char codigo[4]; C01
+    int populacao; 2.429000
+    float area; 313,8 km²
+    float pib;  83.459000
+    int pontos_turisticos; 15
+  
+ char estado[50]; Ceará
+    char nome[50]; Sobral
+    char codigo[4]; C02
+    int populacao; 242.216
+    float area; 2 068,474 km²
+    float pib;  21 679,33
+    int pontos_turisticos; 10
+
+    char estado[50]; Ceará
+   char nome[50]; Acaraú 
+    char codigo[4]; C03
+    int populacao; 68 758 
+    float area; 842,884 km²
+    float pib; 309 490,000 
+    int pontos_turisticos; 5
+
+    char estado[50]; Ceará
+    char nome[50]; Maracanaú
+    char codigo[4]; C04
+    int populacao; 234.509
+    float area; 105,071 km²
+    float pib; 6.500000000 
+    int pontos_turisticos; 5
+
+
+
+
+   char estado[50]; Distrito Federal
+    char nome[50]; Brasília
+    char codigo[4]; D01
+    int populacao; 2.915869
+    float area; 5.801 km²
+    float pib; 34.959000
+    int pontos_turisticos; 15
+
+   char estado[50]; Distrito Federal
+    char nome[50]; Planaltina
+    char codigo[4]; D02
+    int populacao; 93.872
+    float area; 1.194 km²
+    float pib; 1.500000000
+    int pontos_turisticos; 5
+
+   char estado[50]; Distrito Federal
+    char nome[50]; Águas Lindas de Goiás
+    char codigo[4]; D03
+    int populacao; 225.693 
+    float area; 191,817 km²
+    float pib; 11.052,96
+    int pontos_turisticos; 5
+
+    char estado[50]; Distrito Federal
+    char nome[50]; Sobradinho
+    char codigo[4]; D04
+    int populacao; 91.142
+    float area; 1.142 km²
+    float pib; 1.500000000
+    int pontos_turisticos; 5
+
+
+
+ 
+    char estado[50]; Espírito Santo
+    char nome[50]; Vitória
+    char codigo[4]; E01
+    int populacao;  396.718
+    float area; 1.103 km²
+    float pib; 14.500000000
+    int pontos_turisticos; 11
+  
+    char estado[50]; Espírito Santo
+    char nome[50]; Serra
+    char codigo[4]; E02
+    int populacao; 520.653
+    float area; 547,6 km²
+    float pib; 37270000000
+    int pontos_turisticos; 8
+ 
+    char estado[50];Espírito Santo
+    char nome[50]; Cariacica
+    char codigo[4]; E03
+    int populacao; 393.946
+    float area; 279,975 km²
+    float pib; 13 698 973
+    int pontos_turisticos; 5
+ 
+    char estado[50]; Espírito Santo
+    char nome[50]; Guarapari
+    char codigo[4]; E04
+    int populacao; 124.656
+    float area; 589,826 km²
+    float pib; 19.746,25
+    int pontos_turisticos; 7
+
+     
+
+
+    char estado[50]; Goiás
+    char nome[50]; Goiânia
+    char codigo[4]; G01
+    int populacao; 1.444919
+    float area; 729,296 km²
+    float pib; 59.000000000
+    int pontos_turisticos; 20
+ 
+   char estado[50]; Goiás
+    char nome[50]; Anápolis
+    char codigo[4]; G02
+    int populacao; 434.1111
+    float area; 933,156 km²
+    float pib; 44.860,34
+    int pontos_turisticos; 10
+
+    char estado[50]; Goiás
+   char nome[50]; Aparecida de Goiânia
+    char codigo[4]; G03
+    int populacao; 550.925
+    float area; 279,954 km²
+    float pib; 28.213,26
+    int pontos_turisticos; 10
+ 
+    char estado[50]; Goiás
+    char nome[50]; Trindade
+    char codigo[4]; G04
+    int populacao; 104.111
+    float area; 712,690
+    float pib; 20.200,76
+    int pontos_turisticos; 15
+  
+
+
+
+   char estado[50]; Maranhão
+    char nome[50]; São Luís
+    char codigo[4]; M01
+    int populacao; 1.011415
+    float area; 582,974 km²
+    float pib; 12.000,00
+    int pontos_turisticos; 10
+
+    char estado[50]; Maranhão
+    char nome[50]; Imperatriz
+    char codigo[4]; M02
+    int populacao; 250.000
+    float area; 1.000,000 km²
+    float pib; 10.000,00
+    int pontos_turisticos;  5
+ 
+   char estado[50]; Maranhão
+    char nome[50]; Caxias
+    char codigo[4]; M03
+    int populacao; 463.501
+    float area; 1.652 km²
+    float pib; 1.814096000
+    int pontos_turisticos; 9
+
+   char estado[50]; Maranhão
+    char nome[50]; São José de Ribamar
+    char codigo[4]; M04
+    int populacao; 244.579
+    float area;  386,282 km²
+    float pib;	12.719,88
+    int pontos_turisticos; 15
+
+
+
+
+   char estado[50]; Rio de Janeiro
+    char nome[50]; Rio de Janeiro
+    char codigo[4]; R01 
+    int populacao; 6.775561
+    float area; 1.221,040 km²
+    float pib; 73.300,00
+    int pontos_turisticos; 20
+
+    char estado[50]; Rio de Janeiro
+   char nome[50]; Niterói
+    char codigo[4]; R02
+    int populacao; 487.327
+    float area; 138,000 km²
+    float pib; 10.000,00
+    int pontos_turisticos; 10
+
+    char estado[50]; Rio de Janeiro
+   char nome[50]; Nova Iguaçu
+    char codigo[4]; R03
+    int populacao;  1.363111
+    float area;  138,000 km²
+    float pib;  21.559,06
+    int pontos_turisticos; 5
+
+    char estado[50]; Rio de Janeiro
+    char nome[50];  Duque de Caxias
+    char codigo[4]; R04
+    int populacao;  934.914
+    float area; 118,000 km²
+    float pib;  12.000,00
+    int pontos_turisticos; 5
+
+};
+
+int main() {
+    // Criando um array de cidades para armazenar os dados
+    struct Cidade cidades[10];  // Ajuste o tamanho conforme necessário
+    int num_cidades = 0; // Contador de cidades
+
+    // Exemplo de entrada de dados para uma cidade
+    printf("Insira os dados para uma cidade:\n");
+
+    printf("Estado: ");
+    scanf("%s", cidades[num_cidades].estado);
+
+    printf("Nome da cidade: ");
+    scanf("%s", cidades[num_cidades].nome);
+
+    printf("Código da cidade: ");
+    scanf("%s", cidades[num_cidades].codigo);
+
+    printf("População: ");
+    scanf("%d", &cidades[num_cidades].populacao);
 
     printf("Área (em km²): ");
-    scanf("%f", &cidade,area);
+    scanf("%f", &cidades[num_cidades].area);
 
     printf("PIB (em bilhões): ");
-    scanf("%f", &cidade,pib);
+    scanf("%f", &cidades[num_cidades].pib);
 
     printf("Número de pontos turísticos: ");
-    scanf("%d", &cidade,pontosTuristicos);
+    scanf("%d", &cidades[num_cidades].pontos_turisticos);
 
-    printf("Cidade %s cadastrada com sucesso!\n\n", cidade,nome);
+    printf("Cidade %s cadastrada com sucesso!\n\n", cidades[num_cidades].nome);
+    num_cidades++;
 
-
-
-     printf("\nDados das cidades cadastradas:\n");
-      printf("\n");
-
-  printf("Nome: %s\n", cidades[i].nome);
+    // Exibir as cidades cadastradas
+    printf("\nDados das cidades cadastradas:\n");
+    for (int i = 0; i < num_cidades; i++) {
+        printf("\nNome: %s\n", cidades[i].nome);
+        printf("Estado: %s\n", cidades[i].estado);
         printf("Código: %s\n", cidades[i].codigo);
         printf("População: %d\n", cidades[i].populacao);
         printf("Área: %.2f km²\n", cidades[i].area);
         printf("PIB: %.2f bilhões\n", cidades[i].pib);
-        printf("Pontos Turísticos: %d\n", cidades[i].pontosTuristicos);
+        printf("Pontos Turísticos: %d\n", cidades[i].pontos_turisticos);
         printf("\n");
+    }
+
     return 0;
-
-
 }
